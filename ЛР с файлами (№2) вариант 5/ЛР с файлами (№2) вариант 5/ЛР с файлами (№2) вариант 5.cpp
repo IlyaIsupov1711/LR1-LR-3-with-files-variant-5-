d3dd0813655114fbebe33dacf5f25c1dd0df0d65
Admin <isupovilya1711@yandex.ru>
2023-01-31 18:13:54 +0300

    1.0

---
+++ ЛР с файлами (№2) вариант 5/ЛР с файлами (№2) вариант 5/ЛР с файлами (№2) вариант 5.cpp
@@ -0,0 +1,107 @@
+﻿#include <string>
+#include <cstdlib>
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+class foot 
+{
+private:
+	string name;	  //Имена нападающих
+	int num_of_shaib; //Количество заброшенных шайб
+	int num_of_pered; //Количество сделанных голевых передач
+	int straf_time;   //Заработанное штрафное время
+public:
+	void Set(string name, int num_of_shaib, int num_of_pered, int straf_time) //Set для приватных полей класса
+	{
+		this->name = name;
+		this->num_of_shaib = num_of_shaib;
+		this->num_of_pered = num_of_pered;
+		this->straf_time = straf_time;
+	}
+	int get_straf_time()
+	{
+		return straf_time;
+	}
+	int get_num_of_pered()
+	{
+		return num_of_pered;
+	}
+	string get_name()
+	{
+		return name;
+	}
+	int get_num_of_shaib()
+	{
+		return num_of_shaib;
+	}
+	int raz()
+	{
+		return num_of_shaib + num_of_pered;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	foot foot_cl;
+	ifstream file("file.txt"); //Присваивание файла к переменной
+	if (!file) //Проверка на подключение файла
+	{
+		cerr << "Неполучается открыть файл" << endl;
+		exit(1);
+	}
+	string line;
+	if (!file.good())
+	{
+		cout << "Файл не открыт!" << endl;
+	}
+	else
+	{
+		int num;
+		cout << "Введите число футболистов: ";
+		cin >> num;
+		int mass2[7];
+		int mass_of_shaib[7];
+		int mass_of_pered[7];
+		int mass_of_rez[7];
+		string mass_name[7];
+		int max1 = 0, max2 = 0, max3 = 0, max4 = 0;
+		for (int i = 0; i<num+1; i++)
+		{
+			getline(file, line);
+			istringstream ss(line);
+			string name;
+			int num_of_goals, num_of_pered, straf_time;
+			ss >> name >> num_of_goals >> num_of_pered >> straf_time;
+			foot_cl.Set(name, num_of_goals, num_of_pered, straf_time);
+			mass2[i] = foot_cl.get_straf_time();
+			mass_name[i] = foot_cl.get_name();
+			mass_of_shaib[i] = foot_cl.get_num_of_shaib();
+			mass_of_pered[i] = foot_cl.get_num_of_pered();
+			mass_of_rez[i] = foot_cl.raz();
+			if (mass_of_rez[i] > max1)
+			{
+				max1 = mass_of_rez[i];
+				cout << "Сумма: " << max1 << " " << "Имя: " << mass_name[i] << " " << "Голы: " << mass_of_shaib[i] << " " << "Голевые передачи: " << mass_of_pered[i] << endl;
+			}
+			if (mass_of_rez[i] > 10 && mass_of_rez[i] < max1)
+			{
+				max2 = mass_of_rez[i];
+				cout << "Сумма: " << max2 << " " << "Имя: " << mass_name[i] << " " << "Голы: " << mass_of_shaib[i] << " " << "Голевые передачи: " << mass_of_pered[i] << endl;
+			}
+			if (mass_of_rez[i] < max2 && mass_of_rez[i]>10)
+			{
+				max3 = mass_of_rez[i];
+				cout << "Сумма: " << max3 << " " << "Имя: " << mass_name[i] << " " << "Голы: " << mass_of_shaib[i] << " " << "Голевые передачи: " << mass_of_pered[i] << endl;
+			}
+		}
+	}
+	file.close();
+	return 0;
+}
---
+++ ЛР с файлами №3 вариант 5/ЛР с файлами №3 вариант 5/ЛР с файлами №3 вариант 5.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <cstdlib>
+#include <string>
+
+int main()
+{
+	using namespace std;
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	fstream file("file.txt");
+	if (!file)
+	{
+		cout << "Файл не открыт!" << endl;
+		exit(1);
+	}
+	int n;
+	cout << "Введите количество чисел в файле: ";
+	cin >> n;
+	int counter = 0;
+	bool yes_no = true;
+	for (int i=0; i<n; i++)
+	{
+		string str;
+		file >> str;
+		int num;
+		try
+		{
+			num = stoi(str);
+			if (num > 0 && num % 2 == 0)
+			{
+				counter = counter + 1;
+			}
+		}
+		catch (exception e)
+		{
+			yes_no = false;
+			cout << "Ошибка" << endl;
+			break;
+		}
+		//cout << str << endl;
+	}
+	if (yes_no == true)
+	{
+		cout << "Количество четных положительных чисел: " << counter << endl;
+	}
+}
